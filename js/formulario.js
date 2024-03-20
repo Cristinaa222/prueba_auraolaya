aca061ba31c7906411cd4609290f43397d08703d
Aura Olaya <olayacristina01@gmail.com>
2024-03-20 11:38:03 -0500

    actualizacion crear paciente

--- js/formulario.js
+++ js/formulario.js
@@ -2,22 +2,19 @@ const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
-	usuario: /^\d{7,11}$/, 
-	nombre: /^[a-zA-ZÀ-ÿ\s]{15,40}$/,
+	documentos: /^\d{7,11}$/, 
+	nombres: /^[a-zA-ZÀ-ÿ\s]{15,20}$/,
 	edad: /^\d{1,3}$/,
-	telefono: /^\d{7,14}$/, 
-	password: /^.{8,12}$/, 
+	telefono: /^\d{7,14}$/,  
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 
 	
 }
 
 const campos = {
-	usuario: false,
-	nombre: false,
+	documento: false,
+	nombres: false,
 	edad: false,
-	telefono: false,
-	password: false,
 	correo: false,
 
 	
@@ -25,25 +22,16 @@ const campos = {
 
 const validarFormulario = (e) => {
 	switch (e.target.name) {
-		case "usuario":
-			validarCampo(expresiones.usuario, e.target, 'usuario');
+		case "documento":
+			validarCampo(expresiones.documento, e.target, 'documento');
 		break;
-		case "nombre":
-			validarCampo(expresiones.nombre, e.target, 'nombre');
+		case "nombres":
+			validarCampo(expresiones.nombres, e.target, 'nombres');
 		break;
 		case "edad":
 			validarCampo(expresiones.edad, e.target, 'edad');
 		break;
-		case "telefono":
-			validarCampo(expresiones.telefono, e.target, 'telefono');
-		break;
-		case "password":
-			validarCampo(expresiones.password, e.target, 'password');
-			validarPassword2();
-		break;
-		case "password2":
-			validarPassword2();
-		break;
+		
 		case "correo":
 			validarCampo(expresiones.correo, e.target, 'correo');
 		break;
@@ -69,27 +57,6 @@ const validarCampo = (expresion, input, campo) => {
 	}
 }
 
-const validarPassword2 = () => {
-	const inputPassword1 = document.getElementById('password');
-	const inputPassword2 = document.getElementById('password2');
-
-	if(inputPassword1.value !== inputPassword2.value){
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
-		campos['password'] = false;
-	} else {
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
-		campos['password'] = true;
-	}
-}
-
 inputs.forEach((input) => {
 	input.addEventListener('keyup', validarFormulario);
 	input.addEventListener('blur', validarFormulario);
@@ -97,19 +64,17 @@ inputs.forEach((input) => {
 
 formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
-		var doc = document.getElementById('usuario').value;
-		var nom = document.getElementById('nombre').value;
+		var doc = document.getElementById('documento').value;
+		var nom = document.getElementById('nombres').value;
 		var edad = document.getElementById('edad').value;
-		var telefono = document.getElementById('telefono').value;
-		var pas = document.getElementById('password').value;
-		var email = document.getElementById('correo').value;
-		var tip_usu = document.getElementById('id_tip_use').value;
+		var correo = document.getElementById('correo').value;
+		
 
 	const terminos = document.getElementById('terminos');
-	if(campos.usuario && campos.nombre &&  campos.password && campos.correo  && terminos.checked ){
+	if(campos.documento && campos.nombres && campos.correo  && terminos.checked ){
 		formulario.reset();
-		console.log(doc);console.log(nom);console.log(edad);console.log(telefono);console.log(edad);console.log(telefono);console.log(pas);console.log(email);console.log(tip_usu);
-		$.post ("registro.php?cod=datos",{doc: doc, nom: nom, edad: edad, telefono: telefono, pas: pas, email: email, tip_usu: tip_usu}, function(document){$("#mensaje").html(document);
+		console.log(documento);console.log(nombres);console.log(edad);console.log(edad);console.log(correo);
+		$.post ("registro.php?cod=datos",{documento: documento, nombres: nommbres, edad: edad,  correo: correo}, function(document){$("#mensaje").html(document);
 		
 		}),
 		
